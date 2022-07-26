4cb01be928159040c95391aa4263c0e3cf466b17
Aaditya Chandrasekhar <aadityac@gmail.com>
2022-07-26 05:21:48 +0000

    Guard EXT2_ECOMPR_FL usage as it is moved to private API in modern linux kernel headers.

--- xar/lib/ext2.c
+++ xar/lib/ext2.c
@@ -139,8 +139,10 @@ int xar_ext2attr_archive(xar_t x, xar_file_t f, const char* file, const char *bu
 	if(! (flags & ~EXT2_NOCOMPR_FL) )
 		x_addprop(f, "NoCompBlock");
 #endif
+#ifdef EXT2_ECOMPR_FL
 	if(! (flags & ~EXT2_ECOMPR_FL) )
 		x_addprop(f, "CompError");
+#endif
 	if(! (flags & ~EXT2_BTREE_FL) )
 		x_addprop(f, "BTree");
 	if(! (flags & ~EXT2_INDEX_FL) )
@@ -225,8 +227,10 @@ int xar_ext2attr_extract(xar_t x, xar_file_t f, const char* file, char *buffer, 
 	if( e2prop_get(f, "NoCompBlock", (char **)&tmp) == 0 )
 		flags |= EXT2_NOCOMPR_FL ;
 #endif
+#ifdef EXT2_ECOMPR_FL
 	if( e2prop_get(f, "CompError", (char **)&tmp) == 0 )
 		flags |= EXT2_ECOMPR_FL ;
+#endif
 	if( e2prop_get(f, "BTree", (char **)&tmp) == 0 )
 		flags |= EXT2_BTREE_FL ;
 	if( e2prop_get(f, "HashIndexed", (char **)&tmp) == 0 )
